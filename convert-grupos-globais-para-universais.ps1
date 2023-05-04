553615371a6c9fcff359d210f6a3ec1967389e78
Marcelo Abreu <66884595+desigua@users.noreply.github.com>
2023-05-04 12:46:53 -0300

    Converte-Grupos-Globais-Para-Grupos-Universais

---
+++ convert-grupos-globais-para-universais.ps1
@@ -0,0 +1,16 @@
+﻿
+import-module activedirectory
+
+# Busca todos os grupos globais no domínio
+$globalGroups = Get-ADGroup -Filter { GroupScope -eq 'Global' } -SearchBase "ou=grupos,dc=labvmware,dc=local"
+
+# Verifica se existem grupos globais
+if ($globalGroups.Count -eq 0) {
+    Write-Host "Nenhum grupo global encontrado no domínio." -ForegroundColor Yellow
+} else {
+    # Converte os grupos globais para universais
+    foreach ($group in $globalGroups) {
+        Set-ADGroup -Identity $group -GroupScope Universal
+        Write-Host "Grupo $($group.Name) convertido para universal." -ForegroundColor Green
+    }
+}
